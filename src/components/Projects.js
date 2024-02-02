27953b7a7dbe1fae18248f3cf2e439cdc8157882
kfir571 <83332014+kfir571@users.noreply.github.com>
2024-02-02 17:35:34 +0200

    Add Projects 1.0

---
+++ src/components/Projects.js
@@ -0,0 +1,87 @@
+import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+export const Projects = () => {
+
+    const projects = [
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg2,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg1,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg2,
+        },
+        {
+          title: "Business Startup",
+          description: "Design & Development",
+          imgUrl: projImg3,
+        },
+      ];
+
+      return(
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>Projects</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                        <Nav variant="pills" defaultActiveKey="/home">
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Tab One</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">Tub Two</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="third">
+                                Tub Three
+                                </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">
+                                <Row>
+                                    {
+                                        projects.map((project, index) => {
+                                            return (
+                                                <ProjectCard
+                                                key={index}
+                                                {...project}
+                                                />
+                                            )
+                                        })
+                                    }
+                                </Row>
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="second">Loren Inpum2</Tab.Pane>
+                            <Tab.Pane eventKey="third">Loren Inpum3</Tab.Pane>
+                        </Tab.Content>
+                    </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+      )
+}
