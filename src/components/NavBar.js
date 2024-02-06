eeee8be2485b9a468f3a76a69a71c7840033b869
kfir571 <83332014+kfir571@users.noreply.github.com>
2024-02-06 11:28:32 +0200

    Add tzipy changs

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/kfir-shabi1?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2BOHSzn7RScuJxTQ%2BpKTFZA%3D%3D"><img src={navIcon1} alt="" /></a>
-                <a href="https://github.com/kfir571"><img className="git" src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a target="_blank" href="https://www.linkedin.com/in/kfir-shabi1?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2BOHSzn7RScuJxTQ%2BpKTFZA%3D%3D"><img src={navIcon1} alt="" /></a>
+                <a target="_blank" href="https://github.com/kfir571"><img className="git" src={navIcon2} alt="" /></a>
+                <a target="_blank" href="#"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -40,24 +40,22 @@ export const Projects = () => {
       ];
 
       return(
-        <section className="project" id="project">
+        <section className="project" id="projects">
             <Container>
                 <Row>
                     <Col>
                     <h2>Projects</h2>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                        <Nav variant="pills" defaultActiveKey="/home">
+                        <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                             <Nav.Item>
-                                <Nav.Link eventKey="first">Tab One</Nav.Link>
+                                <Nav.Link eventKey="first">Tab 1</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link eventKey="second">Tub Two</Nav.Link>
+                                <Nav.Link eventKey="second">Tab 2</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link eventKey="third">
-                                Tub Three
-                                </Nav.Link>
+                                <Nav.Link eventKey="third">Tab 3</Nav.Link>
                             </Nav.Item>
                         </Nav>
                         <Tab.Content>
@@ -82,6 +80,7 @@ export const Projects = () => {
                     </Col>
                 </Row>
             </Container>
+            <img className="background-image-right" src={colorSharp2} />
         </section>
       )
 }
