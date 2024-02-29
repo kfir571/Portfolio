85421c2a3148d8a8f60d47befa451661afbddd3b
kfir571 <83332014+kfir571@users.noreply.github.com>
2024-02-29 22:38:42 +0200

    fix bug

--- src/components/ContactEmail.js
+++ src/components/ContactEmail.js
@@ -2,16 +2,11 @@ import myAvatar from "../assets/img/myAvatar.png"
 
 export const ContactEmail = () => {
     const emailAddress = "kfir571@gmail.com"
-    const handleEmailContact = () => {
-        console.log("email sent");
-        window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(emailAddress)}`, '_blank');
-
-    }
 
     return(
         <section className="conactEmail" id="conactEmail">
             <h3>
-                <img id="avatar-img" src={myAvatar} alt="My Avatar" />  Delighted you've journeyed this far! Now, let's dive into conversation – <a href="#" onClick={handleEmailContact}>Kfir Shabi</a> awaits!
+                <img id="avatar-img" src={myAvatar} alt="My Avatar" />  Delighted you've journeyed this far! Now, let's dive into conversation – <a href={`https://mail.google.com/mail/?view=cm&fs=1&to=${encodeURIComponent(emailAddress)}`} target="_blank" rel="noreferrer" >Kfir Shabi</a> awaits!
             </h3>
         </section>
     )
